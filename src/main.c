7999b235ebc43b144716be20a84ae8ea6215b968
werley <eduardowerley@gmail.com>
2022-10-01 15:10:13 -0300

    Implement line read

--- src/main.c
+++ src/main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <cJSON/cJSON.h>
 
 int main() {
 
+    const char *filepath = "../resources/records.txt";
     const char record1[] = "{ \"key1\": \"value\", \"key2\": 42, \"key3\": true}";
     const char record2[] = "{\"sadsf\":\"dsewtew\", \"dsre\":3221, \"sdfds\":\"dsfewew\"}";
     int status = 0;
     char *string = NULL;
+
+    FILE *fp = fopen(filepath, "r");
+    if(fp == NULL) {
+        fprintf(stderr, "Unable to open file \"%s\"\n", filepath);
+        return 1;
+    }
+
+    /* Read lines */
+    char *line = NULL;
+    size_t len = 0;
+    while(getline(&line, &len, fp) != -1) {
+        printf("line length: %zd\n", strlen(line));
+    }
+    fclose(fp);
+    exit(0);
 
     /* Parse JSON from record1 */
     cJSON *json = cJSON_Parse(record1);
