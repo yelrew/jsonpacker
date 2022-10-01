c6693735f7dcc612fcee360fda8f3cbe2d67126e
werley <eduardowerley@gmail.com>
2022-10-01 15:42:30 -0300

    main: print JSON records from file

--- src/main.c
+++ src/main.c
@@ -6,46 +6,43 @@
 int main() {
 
     const char *filepath = "../resources/records.txt";
-    const char record1[] = "{ \"key1\": \"value\", \"key2\": 42, \"key3\": true}";
-    const char record2[] = "{\"sadsf\":\"dsewtew\", \"dsre\":3221, \"sdfds\":\"dsfewew\"}";
-    int status = 0;
     char *string = NULL;
+    int status = 0;
 
+    /* Open file with JSON records */
     FILE *fp = fopen(filepath, "r");
     if(fp == NULL) {
         fprintf(stderr, "Unable to open file \"%s\"\n", filepath);
         return 1;
     }
 
-    /* Read lines */
-    char *line = NULL;
+    /* Parse records */
+    char *record = NULL;
     size_t len = 0;
-    while(getline(&line, &len, fp) != -1) {
-        printf("line length: %zd\n", strlen(line));
+    while(getline(&record, &len, fp) != -1) {
+
+        cJSON *json = cJSON_Parse(record);
+        if (json == NULL) {
+            const char *error_ptr = cJSON_GetErrorPtr();
+            if (error_ptr != NULL) {
+                fprintf(stderr, "JSON Syntax Error before: %s\n", error_ptr);
+            }
+            cJSON_Delete(json);
+            status = 1;
+            return status;
+        }
+        /* Print result */
+        string = cJSON_Print(json);
+        if (string == NULL) {
+            fprintf(stderr, "Failed to print json.\n");
+            status = 1;
+            return status;
+        }
+        printf("\"%s\"\n", string);
     }
+
     fclose(fp);
     exit(0);
-
-    /* Parse JSON from record1 */
-    cJSON *json = cJSON_Parse(record1);
-    if (json == NULL) {
-        const char *error_ptr = cJSON_GetErrorPtr();
-        if (error_ptr != NULL) {
-            fprintf(stderr, "JSON Syntax Error before: %s\n", error_ptr);
-        }
-        cJSON_Delete(json);
-        status = 1;
-        return status;
-    }
-
-    /* Print result */
-    string = cJSON_Print(json);
-    if (string == NULL) {
-        fprintf(stderr, "Failed to print json.\n");
-        status = 1;
-        return status;
-    }
-    printf("\"%s\"\n", string);
 
     return status;
 
