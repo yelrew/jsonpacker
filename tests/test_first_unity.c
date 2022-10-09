742594a7ac301c0e76a91d35f84b00000ceddb68
werley <eduardowerley@gmail.com>
2022-10-09 12:55:28 -0300

    Finish test_first_unity

--- tests/test_first_unity.c
+++ tests/test_first_unity.c
@@ -1,8 +1,26 @@
+#include "unity.h"
 #include <stdio.h>
+
+
+int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c)
+{
+    return (int8_t)(((int16_t)a + (int16_t)b + (int16_t)c) / 3);
+}
+
+void test_AverageThreeBytes_should_AverageHighValues()
+{
+    TEST_ASSERT_EQUAL_HEX8(80, AverageThreeBytes(50, 80, 90));
+    TEST_ASSERT_EQUAL_HEX8(127, AverageThreeBytes(127, 127, 127));
+    TEST_ASSERT_EQUAL_HEX8(84, AverageThreeBytes(0, 126, 126));
+}
+
+void setUp(void) {}
+void tearDown(void) {}
 
 int test_first_unity() {
 
-    printf("This is my first test!!");
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+    return UNITY_END();
 
 }
--- tests/test_jsonp_asn1.c
+++ tests/test_jsonp_asn1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void setUp(void) {}
+void tearDown(void) {}
+
 int test_jsonp_asn1() {
 
     printf("This is my first test!!");
